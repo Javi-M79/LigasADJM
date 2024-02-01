5cf4e7818ab8f670c8734ca5ec1f573e69f54090
Silx79 <javier.mira46@gmail.com>
2024-02-01 12:07:02 +0100

    Creada la clase Liga.java

--- src/main/java/model/Liga.java
+++ src/main/java/model/Liga.java
@@ -1,6 +1,35 @@
 package model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Liga {
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+
+@Entity
+public class Liga implements Serializable {
+
+    @Id
+    @Column (name  = "id_liga")
+    private int id;
+    @Column
+    private String nombre_liga;
+    @Column
+    private String fecha_inicio;
+    @Column
+    private String fecha_fin;
+
+
+
 }
