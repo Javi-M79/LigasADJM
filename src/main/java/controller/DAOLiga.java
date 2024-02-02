fe2b1ebe677b8237286ead29f182b84930bae59a
Silx79 <javier.mira46@gmail.com>
2024-02-02 17:45:13 +0100

    Incluido metodo insertarLiga en DB

--- src/main/java/controller/DAOLiga.java
+++ src/main/java/controller/DAOLiga.java
@@ -1,4 +1,39 @@
 package controller;
 
+import database.HibernateUtil;
+import model.Liga;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DAOLiga {
+
+    private SessionFactory sessionFactory;
+//    private ArrayList<Liga> listaLigas = new ArrayList<>();
+
+
+    public DAOLiga() {
+
+        sessionFactory = HibernateUtil.getSessionFactory();
+
+    }
+
+    public void insertarLiga(Liga liga) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        session.persist(liga);
+//        listaLigas.add(liga);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+    }
+
+
 }
