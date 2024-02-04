279867a172d7ea632e7d0838577945aa7d6b953b
Silx79 <javier.mira46@gmail.com>
2024-02-04 08:32:17 +0100

    Pruebas insertar en DB

---
+++ src/main/java/Entrada.java
@@ -0,0 +1,30 @@
+import controller.DAOEquipo;
+import controller.DAOLiga;
+import controller.DAOPartido;
+import model.Equipo;
+import model.Liga;
+
+public class Entrada {
+
+
+    public static void main(String[] args) {
+
+
+        DAOEquipo operacionesEquipo = new DAOEquipo();
+        DAOLiga operacionesLigas = new DAOLiga();
+        DAOPartido operacionesPartido = new DAOPartido();
+
+//        operacionesEquipo.insertarEquipo(new Equipo("Atletico de Madrid", "Madrid"));
+//        operacionesEquipo.insertarEquipo(new Equipo("Real Madrid", "Madrid"));
+//        operacionesEquipo.insertarEquipo(new Equipo("Rayo Vallecano", "Madrid"));
+//        operacionesEquipo.insertarEquipo(new Equipo("Girona F.C", "Girona"));
+//        operacionesEquipo.insertarEquipo(new Equipo("Real Betis Balompie", "Sevilla", (new DatosEquipo("Benito Villamarin", "https://www.realbetisbalompie.es/"))));
+
+
+
+
+
+    }
+
+
+}
