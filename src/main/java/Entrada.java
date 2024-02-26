b759a54c5cf73b70373f43f5b7184d13f5d84626
Silx79 <javier.mira46@gmail.com>
2024-02-26 13:34:47 +0100

    Implementando metodos

--- src/main/java/Entrada.java
+++ src/main/java/Entrada.java
@@ -5,6 +5,7 @@ import model.DatosEquipo;
 import model.Equipo;
 import model.Liga;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Entrada {
@@ -16,17 +17,19 @@ public class Entrada {
         DAOEquipo operacionesEquipo = new DAOEquipo();
         DAOLiga operacionesLigas = new DAOLiga();
         DAOPartido operacionesPartido = new DAOPartido();
-
+        Equipo equipo1 = new Equipo("Real Madrid", "Madrid");
 
 //        operacionesEquipo.insertarEquipo(new Equipo("Atletico de Madrid", "Madrid"));
 //        operacionesEquipo.insertarEquipo(new Equipo("Real Madrid", "Madrid"));
 //        operacionesEquipo.insertarEquipo(new Equipo("Rayo Vallecano", "Madrid"));
 //        operacionesEquipo.insertarEquipo(new Equipo("Girona F.C", "Girona"));
 //        operacionesEquipo.insertarEquipo(new Equipo("Real Betis Balompie", "Sevilla", (new DatosEquipo("Benito Villamarin", "https://www.realbetisbalompie.es/"))));
-//        operacionesLigas.insertarLiga(new Liga("Premier League", "Abril", "Junio"));
+        operacionesLigas.insertarLiga(new Liga("Premier League", "Abril", "Junio"));
 //        operacionesEquipo.equipoALiga(new Equipo("Manchester United", "Manchester", (new DatosEquipo("Old Trafford" ,"https://www.manutd.com/es/" ))), (new Liga("Premier Leage2", "Enero", "Mayo")));
 //        operacionesEquipo.equipoALiga(new Equipo("Real Madrid", "Madrid"), (new Liga("Liga EA", "Agosto", "Mayo")));
-        operacionesLigas.verEquiposliga("Liga EA");
+////        operacionesLigas.verEquiposliga("Liga EA");
+//
+//        operacionesEquipo.eliminarEquipo(equipo1);
 
     }
 
