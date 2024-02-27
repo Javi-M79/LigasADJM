e4a7d79f9a9490a9e0f7bf91fdb47fd322d76735
Silx79 <javier.mira46@gmail.com>
2024-02-27 20:01:50 +0100

    Creado metodo listar equipos

--- src/main/java/controller/DAOEquipo.java
+++ src/main/java/controller/DAOEquipo.java
@@ -6,12 +6,13 @@ import model.Equipo;
 import model.Liga;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import controller.DAOLiga;
 
 import java.util.List;
 
-public class DAOEquipo {
+public class DAOEquipo extends DAOLiga {
 
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
     public DAOEquipo() {
 
@@ -23,35 +24,46 @@ public class DAOEquipo {
 
     public void insertarEquipo(Equipo equipo) {
 
-        //Creamos una sesion
-        Session session = sessionFactory.openSession();
-
-        //Comprobamos que el equipo no este creado.
-
-        //Si no esta creado lo creo.
+        Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
-        session.persist(equipo);
+
+        //Comprobamos si el equipo existe.
+
+        Query query = session.createQuery("FROM Equipo WHERE nombre = :nombre");
+        query.setParameter("nombre", equipo.getNombre());
+        List<Equipo> equiposCreados = query.getResultList();
+        //Si no existe lo creo.
+        if (equiposCreados.isEmpty()) {
+            session.persist(equipo);
+            session.getTransaction().commit();
+            System.out.println("Equipo creado con exito.");
+        } else {
+            System.out.println("El equipo ya existe en la base de datos.");
+        }
+        session.close();
+    }
+
+    //OBTENER EQUIPO
+
+    public void getEquipos() {
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("Select e from Equipo e");
+        List<Equipo> equipo = (List<Equipo>) ((org.hibernate.query.Query<?>) query).list();
+
+        for (Equipo e : equipo
+        ) {
+            System.out.println(e.getId());
+            System.out.println(e.getNombre());
+            System.out.println(e.getCiudad());
+            System.out.println(e.getLiga());
+        }
+
         session.getTransaction().commit();
         session.close();
 
 
-    }
-
-
-    public void eliminarEquipo(Equipo equipo) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        try {
-            session.delete(equipo);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
     }
 
 
@@ -60,27 +72,45 @@ public class DAOEquipo {
 
         Session session = sessionFactory.openSession();
         session.beginTransaction();
+        //Comprobamos que el equipo exista si no exiate lo crea.
+
+
+        insertarEquipo(equipo);
+        insertarLiga(liga);
+
 
         if (equipo.getLiga() != null) {
             System.out.println("El equipo ya pertenece una liga.");
             session.getTransaction().rollback();
             session.close();
+        } else {
+            equipo.setLiga(liga);
+            session.persist(equipo);
+            session.getTransaction().commit();
+            session.close();
         }
-        equipo.setLiga(liga);
-        session.persist(equipo);
-        session.getTransaction().commit();
-        session.close();
     }
 
-    public boolean comprobarEquipo (Equipo equipo){
-        boolean encontrado = true;
+    //COMPROBAR SI EL EQUIPO EXISTE
+    public void comprobarEquipo(Equipo equipo) {
 
         Session session = sessionFactory.openSession();
-        Query query = session.createQuery("Select e from equipos e ");
-        List <Equipo> equipos = query.getResultList();
+        session.beginTransaction();
+        Query query = session.createQuery("FROM e equipos e ");
+        List<Equipo> equipos = query.getResultList(); //No me funciona el metodo List.
+
+        if (equipos.isEmpty()) {
+
+            session.persist(equipo);
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Equipo creado con exito.");
+        } else {
+            System.out.println("El equipo ya existe en la Base de Datos.");
+
+        }
 
 
-        return encontrado;
     }
 
 }
