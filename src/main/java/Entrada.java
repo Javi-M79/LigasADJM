74a03ed180f4d6126d3c98a06b2ed003b1cddbb6
Silx79 <javier.mira46@gmail.com>
2024-02-29 20:25:45 +0100

    Preparando las pruebas

--- src/main/java/Entrada.java
+++ src/main/java/Entrada.java
@@ -4,6 +4,7 @@ import controller.DAOPartido;
 import model.DatosEquipo;
 import model.Equipo;
 import model.Liga;
+import model.Partido;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,116 +19,74 @@ public class Entrada {
         DAOLiga operacionesLigas = new DAOLiga();
         DAOPartido operacionesPartido = new DAOPartido();
 
+
+        /*BORRADO DE DATOS DE LA DB
+
+        for (int i = 0; i < 100; i++) {
+            operacionesPartido.borrarPartido(i);
+        }
+        for (int i = 0; i < 100; i++) {
+            operacionesEquipo.borrarEquipo(i);
+        }
+
+
+        for (int i = 0; i < 100; i++) {
+            operacionesLigas.borrarLiga(i);
+        }*/
+
+
         //CREAR UN A LIGA
-//        operacionesLigas.insertarLiga(new Liga("Liga EA Sports ", "Agosto", "Mayo"));
+
+        operacionesLigas.insertarLiga(new Liga("Liga EA Sports ", "Agosto", "Mayo"));
+
+        //CREAR 8 EQUIPOS
+
+        operacionesEquipo.insertarEquipo(new Equipo("Real Madrid", "Madrid"));
+        operacionesEquipo.insertarEquipo(new Equipo("Atletico de Madrid", "Madrid"));
+        operacionesEquipo.insertarEquipo(new Equipo("F.C Barcelona", "Barcelona"));
+        operacionesEquipo.insertarEquipo(new Equipo("Girona", "Girona"));
+        operacionesEquipo.insertarEquipo(new Equipo("Celta de Vigo", " Vigo"));
+        operacionesEquipo.insertarEquipo(new Equipo("Almeria CF", "Almeria"));
+        operacionesEquipo.insertarEquipo(new Equipo("Atlethic de Bilbao", "Bilbao"));
+        operacionesEquipo.insertarEquipo(new Equipo("Real Sociedad", "San Senbastian"));
+
+
+        //AÑADIR LOS EQUIPOS A UNA LIGA
+
+        //Nuevos equipos y se le asignan una liga directamente.
+//        operacionesEquipo.equipoALiga(new Equipo("PSG", "Paris"), new Liga("Ligue 1 Uber Eats", "Junio","Febrero"));
+//        operacionesEquipo.equipoALiga(new Equipo("Manchester United", "Manchester"), new Liga("Premier League", "Enero","Diciembre"));
+
+        //Equipos existentes a los que solo se les inscribe en una liga a traves de su ID.
+//        operacionesEquipo.equipoALigaporId();
+//        operacionesEquipo.equipoALigaporId();
+//        operacionesEquipo.equipoALigaporId();
+//        operacionesEquipo.equipoALigaporId();
+
+
+        //CREAR 6 PARTIDOS
 //
-//        //CREAR 8 EQUIPOS
-//        operacionesEquipo.insertarEquipo(new Equipo("Real Madrid", "Madrid"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Atletico de Madrid", "Madrid"));
-//        operacionesEquipo.insertarEquipo(new Equipo("F.C Barcelona", "Barcelona"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Girona", "Girona"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Celta de Vigo", " Vigo"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Almeria CF" ,"Almeria"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Atlethic de Bilbao", "Bilbao"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Real Sociedad", "San Senbastian"));
-//
-//        //AÑADIR NUEVOS EQUIPOS EQUIPOS A UNA LIGA (COMPRUEBA SI EXISTEN AMBOS.
-//
-//        operacionesEquipo.equipoALiga(new Equipo("Cadiz" , "Cadiz"),(new Liga("Liga EA Sports ", "Agosto", "Mayo")));
-
-        //AÑADIR EQUIPOS A UN ALIGA POR ID. COMRPUEBA QUE EXISTAN AMBOS
-
-        operacionesEquipo.equipoALigaporId(80, 74);
-        operacionesEquipo.equipoALigaporId(81, 74);
+//        operacionesPartido.insertarPartido("2024-8-15" , 90, 91);
+//        operacionesPartido.insertarPartido("2024-8-15" , 92, 93);
+//        operacionesPartido.insertarPartido("2024-8-16" , 94, 95);
+//        operacionesPartido.insertarPartido("2024-9-02" , 96, 97);
+//        operacionesPartido.insertarPartido("2024-9-03" , 90, 97);
+//        operacionesPartido.insertarPartido("2024-9-03" , 90, 98);
 
 
-        //OBTENER DATOS DE UNA LIGA POR NOMBRE
-        operacionesLigas.getLigaNombre("Liga EA Sports ");
-        // OBTENER DATOS DE UNA LIGA POR ID
+        //MOSTRAR DATOS DE TODOS LOS EQUIPOS
 
-        //OBTENER LISTA DE EQUIPOS
-        operacionesEquipo.listaEquipos();
-
-//        Equipo equipo1 = new Equipo("FC Osasuna", "Pamplona");
-//        Equipo equipo2 = new Equipo("Almeria", "Almeria");
-//        operacionesEquipo.equipoALiga(equipo2, liga1);
-//        Equipo equipo3 = new Equipo("Girona FC", "Girona");
-//        Equipo equipo4 = new Equipo("Atletico de Madrid", "Madrid");
-//        Equipo equipo5 = new Equipo("Celta de Vigo", "Vigo");
-//        Equipo equipo6 = new Equipo("Getafe", "Getafe");
-//        Equipo equipo7 = new Equipo("Real Sociedad", "San Sebastian");
-//        Equipo equipo8 = new Equipo("Sevilla", "Sevilla");
-
-
-//        for(int i =65; i<10; i++){
-//            operacionesEquipo.borrarEquipo(i);
-//
-//        }
-//
-//        for(int i =1; i<10; i++){
-//            operacionesLigas.borrarLiga(i);
-//
-//        }
-
-
-//        operacionesEquipo.equipoALiga(equipo1, liga1);
-//        operacionesEquipo.equipoALiga(equipo2, liga1);
-//        operacionesEquipo.equipoALiga(equipo3, liga1);
-//        operacionesEquipo.equipoALiga(equipo4, liga1);
-//        operacionesEquipo.equipoALiga(equipo5, liga1);
-//        operacionesEquipo.equipoALiga(equipo6, liga1);
-//        operacionesEquipo.equipoALiga(equipo7, liga1);
-//        operacionesEquipo.equipoALiga(equipo8, liga1);
-
-
-//        operacionesEquipo.insertarEquipo(new Equipo("Atletico de Madrid", "Madrid"));
-//
-//        operacionesEquipo.insertarEquipo(new Equipo("Rayo Vallecano", "Madrid"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Girona F.C", "Girona"));
-//        operacionesEquipo.insertarEquipo(new Equipo("Real Betis Balompie", "Sevilla", (new DatosEquipo("Benito Villamarin", "https://www.realbetisbalompie.es/"))));
-
-//        operacionesEquipo.equipoALiga(new Equipo("Manchester United", "Manchester", (new DatosEquipo("Old Trafford" ,"https://www.manutd.com/es/" ))), (new Liga("Premier Leage2", "Enero", "Mayo")));
-//        operacionesEquipo.equipoALiga(new Equipo("Real Madrid", "Madrid"), (new Liga("Liga EA", "Agosto", "Mayo")));
-////        operacionesLigas.verEquiposliga("Liga EA");//
-//        operacionesEquipo.eliminarEquipo(equipo1);
-//        operacionesLigas.insertarLiga(new Liga("Liga Fancesa", "Abril", "Junio"));
-//        operacionesEquipo.getEquipos();
-//        operacionesEquipo.getEquipo("Real Madrid");
-//        operacionesEquipo.equipoALiga(new Equipo("Barcelona FC", "Barcelona"), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.equipoALiga(new Equipo("Villareal FC", "Castellon"), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.getEquipoId(30);
 //        operacionesEquipo.listaEquipos();
-//        operacionesLigas.addEquipoLiga((new Equipo("Villareal FC", "Castellon")), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.equipoALiga((new Equipo("Almeria", "Almeria")), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.equipoALiga((new Equipo("Getafe", "Madrid")), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.equipoALiga((new Equipo("Sevilla", "Sevilla")), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.equipoALiga((new Equipo("Osasuna", "Pamplona")), (new Liga("Liga EA", "Agosto", "Mayo")));
-//        operacionesEquipo.listaEquipos();
-////        operacionesLigas.getLiga("Liga EA");
-//        operacionesEquipo.listaEquipos();
-//        operacionesLigas.insertarLiga(new Liga("Bundesliga", "Enero", "Diciembre"));
-//        operacionesLigas.listaLigas();
-//        operacionesLigas.getLiga("Bundesliga");
-//        operacionesLigas.listarEquiposPorLiga(66);
-//        operacionesLigas.listarEquiposPorLiga(36);
-//        operacionesLigas.listarEquiposPorLiga(45);
-//        operacionesLigas.listarEquiposNombre("Liga EA");
-//        operacionesLigas.listarEquiposPorLiga(45);
-//        operacionesLigas.listarEquiposPorLiga(45);
-//        operacionesLigas.getLigaID(66);
-//        operacionesEquipo.borrarEquipo(66);
-//        operacionesEquipo.borrarEquipo(67);
-//        operacionesEquipo.borrarEquipo(63);
-//        operacionesEquipo.borrarEquipo(64);
-//        operacionesEquipo.borrarEquipo(69);
-//        operacionesEquipo.borrarEquipo(70);
-//        operacionesEquipo.borrarEquipo(71);
-//        operacionesEquipo.borrarEquipo(72);
-//        operacionesEquipo.borrarEquipo(68);
+
+        //ELIMINAR DOS EQUIPOS
 //
-//        operacionesLigas.borrarLiga(64);
-//        operacionesLigas.borrarLiga(65);
-//        operacionesLigas.borrarLiga(66);
+//        operacionesEquipo.borrarEquipo(90);
+//        operacionesEquipo.borrarEquipo(91);
+
+
+        //MOSTRAR PARTIDOS DE UNA LIGA CONCRETA
+
+//        operacionesPartido.partidosLiga();
 
 
     }
